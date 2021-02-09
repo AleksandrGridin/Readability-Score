ff25c1a42c9f8dac729b8f9d09d4c51bc5e657ea
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-02-09 16:21:44 +0300

    Stage 2/4

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -1,7 +1,24 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+        try (Scanner sc = new Scanner(System.in)) {
+            String[] line = sc.nextLine().split("[!?.]");
+            int count = 0;
+            for (int i = 0; i < line.length; i++) {
+                count += line[i].split(" ").length;
+            }
+            int average = count / line.length;
+
+            if (average > 10 ) {
+                System.out.println("HARD");
+            } else {
+                System.out.println("EASY");
+            }
+        }
     }
 }
